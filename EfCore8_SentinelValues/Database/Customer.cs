91a303e55c09edd21ddf79aea31015e2de36e98f
Tomasz Prasołek <tomasz.prasolek@gmail.com>
2024-05-11 07:38:47 +0200

    Initial commit

---
+++ EfCore8_SentinelValues/Database/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace EfCore8_SentinelValues.Database;
+
+public sealed class Customer
+{
+    public int Id { get; init; }
+    public Status Status { get; init; }
+    public int Points { get; init; }
+    public DateTime Created { get; init; }
+}
+
+public enum Status
+{
+    New = 1,
+    Verified = 2,
+    Deactivate = 3
+}
---
+++ EfCore8_SentinelValues/Database/MyContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCore8_SentinelValues.Database;
+
+internal class MyContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>()
+            .HasKey(x => x.Id);
+
+        modelBuilder.Entity<Customer>()
+            .Property(x => x.Id);
+
+        modelBuilder.Entity<Customer>()
+            .Property(x => x.Status)
+            .IsRequired()
+            .HasDefaultValue(Status.New);
+
+        modelBuilder.Entity<Customer>()
+            .Property(x => x.Points)
+            .IsRequired()
+            .HasDefaultValue(10);
+
+        modelBuilder.Entity<Customer>()
+            .Property(x => x.Created)
+            .IsRequired()
+            .HasDefaultValue(DateTime.UtcNow);
+    }
+}
